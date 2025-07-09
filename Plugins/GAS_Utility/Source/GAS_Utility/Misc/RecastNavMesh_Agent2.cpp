59d03a3ddd5afc13334732b12c55617361bb3180
CALDRAY Tom <60530081+Zell441000@users.noreply.github.com>
2025-07-09 21:23:21 +0200

    remove plugins 5.3

--- Plugins/GAS_Utility/Source/GAS_Utility/Misc/RecastNavMesh_Agent2.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Misc/RecastNavMesh_Agent2.h"
-
-
-ARecastNavMesh_Agent2::ARecastNavMesh_Agent2(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
-{
-}
